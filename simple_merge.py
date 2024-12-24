71dedf60d9c3aa0d928b46f5a96a226f58582024
Evgenii Rechkalov <snorkysnark@gmail.com>
2024-12-24 15:05:43 +0100

    connection+metadata class

--- simple_merge.py
+++ simple_merge.py
@@ -1,48 +1,53 @@
 from typing import Annotated
 import typer
-from contextlib import ExitStack
+from contextlib import ExitStack, contextmanager
 from sqlalchemy import (
     create_engine,
     Connection,
     Engine,
     MetaData,
-    URL,
-    make_url,
 )
 
 
-def reflect_metadata(source: Engine | Connection) -> MetaData:
-    metadata = MetaData()
-    metadata.reflect(source)
-    return metadata
+class SmartConnection:
+    def __init__(self, connection: Connection) -> None:
+        self.connection = connection
+
+        metadata = MetaData()
+        metadata.reflect(connection)
+        self.metadata = metadata
+
+    @contextmanager
+    @staticmethod
+    def connect(engine: Engine):
+        with engine.connect() as conn:
+            yield SmartConnection(conn)
 
 
-def simple_merge(inputs: list[Connection], output: Connection):
-    for input_connection in inputs:
-        metadata = reflect_metadata(input_connection)
-
-        for table in metadata.sorted_tables:
+def simple_merge(inputs: list[SmartConnection], output: Connection):
+    for input in inputs:
+        for table in input.metadata.sorted_tables:
             print("Inserting", table.key)
             table.create(output, checkfirst=False)  # throw error if already exists
 
             insert = table.insert()
-            for row in input_connection.execute(table.select()):
+            for row in input.connection.execute(table.select()):
                 output.execute(insert, row._asdict())
 
 
 def main(
-    input_urls: list[str],
-    output_url: Annotated[URL, typer.Option("-o", "--output", parser=make_url)],
+    input_urls: list[str], output_url: Annotated[str, typer.Option("-o", "--output")]
 ):
     with ExitStack() as stack:
-        input_connections = [
-            stack.enter_context(create_engine(url).connect()) for url in input_urls
+        inputs = [
+            stack.enter_context(SmartConnection.connect(create_engine(url)))
+            for url in input_urls
         ]
-        output_connection = stack.enter_context(create_engine(output_url).connect())
+        output = stack.enter_context(SmartConnection.connect(create_engine(output_url)))
 
-        reflect_metadata(output_connection).drop_all(output_connection)
-        simple_merge(input_connections, output_connection)
-        output_connection.commit()
+        output.metadata.drop_all(output.connection)
+        simple_merge(inputs, output.connection)
+        output.connection.commit()
 
 
 if __name__ == "__main__":
