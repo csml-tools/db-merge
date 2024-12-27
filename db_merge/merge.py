76ef34615ffa3a43e2318ac10ec1416793038cd9
Evgenii Rechkalov <snorkysnark@gmail.com>
2024-12-27 01:10:21 +0100

    check for same columns/data

--- db_merge/merge.py
+++ db_merge/merge.py
@@ -1,11 +1,12 @@
 from contextlib import ExitStack
 from dataclasses import dataclass
-from typing import Optional
-from sqlalchemy import create_engine
+from typing import Any, Callable, Optional
+from pydantic.json_schema import AnyType
+from sqlalchemy import create_engine, Connection
 from sys import exit
 
 from .options import MergeOptions, TableIdent
-from .reflect import MergedGraph
+from .reflect import MergedGraph, MergedTable, TableSource
 
 
 @dataclass
@@ -32,6 +33,75 @@ def find_overlaps(graph: MergedGraph, options: MergeOptions) -> list[TableIdent]
     ]
 
 
+def copy_single_table(source: TableSource, out_connection: Connection):
+    source.table.create(out_connection, checkfirst=False)
+
+    insert = source.table.insert()
+    for row in source.connection.execute(source.table.select()):
+        out_connection.execute(insert, row._asdict())
+
+
+def check_same_columns(sources: list[TableSource]) -> bool:
+    if len(sources) > 0:
+        first = set(sources[0].table.columns.keys())
+        for i in range(1, len(sources)):
+            if set(sources[i].table.columns.keys()) != first:
+                return False
+
+    return True
+
+
+def check_same_columns_raise(merged_table: MergedTable):
+    if not check_same_columns(merged_table.sources):
+        raise RuntimeError(f"Overlapping {merged_table.ident} has non-matching columns")
+
+
+type Rowdict = dict[tuple, dict]
+
+
+def get_rows_by_primary_key(source: TableSource) -> Rowdict:
+    pk_columns = source.table.primary_key.columns
+    row_by_pk = {}
+
+    for row in source.connection.execute(source.table.select()):
+        rowdict = row._asdict()
+        row_by_pk[tuple(rowdict[column.name] for column in pk_columns)] = rowdict
+
+    return row_by_pk
+
+
+def dicts_equal(
+    d1: dict, d2: dict, *, equality: Callable[[Any, Any], bool] = lambda a, b: a == b
+) -> bool:
+    if len(d1) != len(d2):
+        return False
+
+    for key in d1:
+        if key not in d2 or not equality(d1[key], d2[key]):
+            return False
+
+    return True
+
+
+def check_same_data(sources: list[TableSource]):
+    if len(sources) > 0:
+        first = get_rows_by_primary_key(sources[0])
+        for i in range(1, len(sources)):
+            if not dicts_equal(
+                get_rows_by_primary_key(sources[i]), first, equality=dicts_equal
+            ):
+                return False
+
+    return True
+
+
+def check_same_data_raise(merged_table: MergedTable):
+    if not check_same_data(merged_table.sources):
+        raise RuntimeError(
+            f"Overlapping {merged_table.ident} must have the same data across all sources"
+        )
+
+
 def smart_merge(
     inputs: list[SlicedUrl],
     output_url: str,
@@ -52,3 +122,16 @@ def smart_merge(
             for overlapping in overlaps:
                 print(overlapping)
             exit(1)
+
+        out_connection = stack.enter_context(create_engine(output_url).connect())
+        for merged_table in graph.sort():
+            if len(merged_table.sources) == 1:
+                copy_single_table(merged_table.sources[0], out_connection)
+            else:
+                check_same_columns_raise(merged_table)
+
+                if merged_table.ident in options.same:
+                    check_same_data_raise(merged_table)
+                    copy_single_table(merged_table.sources[0], out_connection)
+                else:
+                    pass
